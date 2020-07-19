6d8cde11e5f3b0ac03880d9f1134cbe7f0779e6f
xuexiaohua001 <xuexiaohua001@ke.com>
2020-07-19 14:24:25 +0800

    计算器测试用例

---
+++ pythoncode/calc.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+# 实现计算机
+class Calculator:
+    def add(self, a, b):
+        return a + b
+
+    def sub(self, a, b):
+        return a - b
+
+    def multi(self, a, b):
+        return a * b
+
+    def div(self, a, b):
+        return a / b
+
+
---
+++ testing/test_calc.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# 测试文件
+
+import sys
+import pytest
+sys.path.append('..')
+
+from pythoncode.calc import Calculator
+
+class TestCalc:
+    @pytest.mark.add
+    @pytest.mark.parametrize('a', [2, 0.2, 0, -0.2, -2])
+    @pytest.mark.parametrize('b', [2, 0.2, 0, -0.2, -2])
+    def test_add(self, a, b):
+        cal = Calculator()
+        assert a + b == cal.add(a, b)
+
+    @pytest.mark.sub
+    @pytest.mark.parametrize('a', [2, 0.2, 0, -0.2, -2])
+    @pytest.mark.parametrize('b', [2, 0.2, 0, -0.2, -2])
+    def test_sub(self, a, b):
+        cal = Calculator()
+        assert a - b == cal.sub(a, b)
+
+    @pytest.mark.multi
+    @pytest.mark.parametrize('a', [2, 0.2, 0, -0.2, -2])
+    @pytest.mark.parametrize('b', [2, 0.2, 0, -0.2, -2])
+    def test_multi(self, a, b):
+        cal = Calculator()
+        assert a * b == cal.multi(a, b)
+
+    @pytest.mark.div
+    @pytest.mark.parametrize('a', [2, 0.2, 0, -0.2, -2])
+    @pytest.mark.parametrize('b', [2, 0.2, 0, -0.2, -2])
+    def test_div(self, a, b):
+        cal = Calculator()
+        assert a / b == cal.div(a, b)
+
+if __name__ == '__main__':
+    pytest.main()
