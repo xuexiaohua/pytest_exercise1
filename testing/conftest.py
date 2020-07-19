8bb28632056dc71179fe2d4b2bcb7867816ce8a0
xuexiaohua001 <xuexiaohua001@ke.com>
2020-07-19 16:10:55 +0800

    修正调用方法后调用打印计算结束

--- testing/conftest.py
+++ testing/conftest.py
@@ -2,9 +2,14 @@
 # -*- coding:utf-8 -*-
 import pytest
 # 函数级的setup\teardown
-@pytest.fixture()
+
 def setup_function():
     print("开始计算")
-@pytest.fixture()
+
 def teardown_function():
     print("计算结束")
+
+@pytest.fixture()
+def forward():
+    yield setup_function()
+    teardown_function()
--- testing/test_calc.py
+++ testing/test_calc.py
@@ -4,32 +4,37 @@
 
 import sys
 import pytest
+
+
+
 sys.path.append('..')
-
 from pythoncode.calc import Calculator
-
 
 class TestCalc:
     @pytest.mark.parametrize('a', [2, 0.2, 0, -0.2, -2])
     @pytest.mark.parametrize('b', [2, 0.2, 0, -0.2, -2])
-    def test_add(self, a, b, setup_function, teardown_function):
+    def test_add(self, a, b, forward):
         cal = Calculator()
         assert a + b == cal.add(a, b)
 
     @pytest.mark.parametrize('a', [2, 0.2, 0, -0.2, -2])
     @pytest.mark.parametrize('b', [2, 0.2, 0, -0.2, -2])
-    def test_sub(self, a, b, setup_function, teardown_function):
+    def test_sub(self, a, b, forward):
         cal = Calculator()
         assert a - b == cal.sub(a, b)
 
     @pytest.mark.parametrize('a', [2, 0.2, 0, -0.2, -2])
     @pytest.mark.parametrize('b', [2, 0.2, 0, -0.2, -2])
-    def test_multi(self, a, b, setup_function, teardown_function):
+    def test_multi(self, a, b, forward):
         cal = Calculator()
         assert a * b == cal.multi(a, b)
 
     @pytest.mark.parametrize('a', [2, 0.2, 0, -0.2, -2])
     @pytest.mark.parametrize('b', [2, 0.2, 0, -0.2, -2])
-    def test_div(self, a, b, setup_function, teardown_function):
+    def test_div(self, a, b, forward):
         cal = Calculator()
         assert a / b == cal.div(a, b)
+
+
+if __name__ == '__main__':
+    pytest.main()
